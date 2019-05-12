Correcting loss calculation and adding data

code/wic.py
@@ -10,7 +10,7 @@ import numpy as np
 from glob import glob
 
 from model2 import NLIModel
-from model import MultiTaskEncoder
+from model import MultiTaskEncoder, get_device
 from data import DatasetHandler as data 
 from mutils import load_model, load_model_from_args, load_args, args_to_params, visualize_tSNE, get_transfer_datasets
 
@@ -18,9 +18,9 @@ from mutils import load_model, load_model_from_args, load_args, args_to_params, 
 
 
 class WICclassifier(nn.Module):
-    def __init__(self, batch_size=64, lstm_dim= 400, fc_dim = 100, n_classes = 2):
+    def __init__(self, batch_size=64, lstm_dim= 400, fc_dim = 100, n_classes = 1):
         super(WICclassifier, self).__init__()
-        self.classifier = nn.Sequential(nn.Linear(lstm_dim*2,fc_dim), nn.ReLU(), nn.Linear(fc_dim,n_classes))
+        self.classifier = nn.Sequential(nn.Linear(lstm_dim*2,fc_dim), nn.ReLU(), nn.Linear(fc_dim,n_classes), nn.Sigmoid())
     def forward(self,inputs):
         return self.classifier(inputs)
         
@@ -33,6 +33,7 @@ class WIC:
         self.encoder = model[0]
         self.classifier = WICclassifier(batch_size= batch_size, lstm_dim=lstm_dim, fc_dim=fc_dim)
         self.batch_size = batch_size
+        self.classifier = self.classifier.to(get_device())
 
     def train(self, iteration=None, dataset=None, ret_pred_list=False):
         
@@ -61,12 +62,13 @@ class WIC:
 
                
                 # encode the sentences batch
-                sent1_encoded = self.encoder.encode_sentence(embeds[0], lengths[0], word_level=True)[1].detach()
-                sent2_encoded = self.encoder.encode_sentence(embeds[1], lengths[1], word_level=True)[1].detach()
+                with torch.no_grad():
+                    sent1_encoded = self.encoder.encode_sentence(embeds[0], lengths[0], word_level=True)[1].detach()
+                    sent2_encoded = self.encoder.encode_sentence(embeds[1], lengths[1], word_level=True)[1].detach()
                 
                 # prepare the input features based on specified word only  
-                bins1 = torch.arange(0, sent1_encoded.size(0)*sent1_encoded.size(1), step = sent1_encoded.size(1))
-                bins2 = torch.arange(0, sent2_encoded.size(0)*sent2_encoded.size(1), step = sent2_encoded.size(1))
+                bins1 = torch.arange(0, sent1_encoded.size(0)*sent1_encoded.size(1), step = sent1_encoded.size(1), device=get_device())
+                bins2 = torch.arange(0, sent2_encoded.size(0)*sent2_encoded.size(1), step = sent2_encoded.size(1), device=get_device())
                 
                 sent1_encoded = sent1_encoded.view(-1, sent1_encoded.size(2))
                 sent2_encoded = sent2_encoded.view(-1, sent2_encoded.size(2))
@@ -88,9 +90,7 @@ class WIC:
 #                 preds = cos_sim/torch.sum(cos_sim)
 
                 # get predictions
-                out = self.classifier(input_features)
-                preds = torch.argmax(out, dim =1).float()
-                
+                out = self.classifier(input_features)[:,0]
 
                 # correct preds
 #                 correct_preds += torch.squeeze(preds == batch_labels).tolist()
@@ -98,7 +98,7 @@ class WIC:
 #                 accuracy.append(acc)
 
                 # calculate loss
-                loss = criterion(preds, batch_labels.float())
+                loss = criterion(out, batch_labels.float())
                 loss_list.append(loss.item())
 
                 # back prop steps
@@ -110,7 +110,7 @@ class WIC:
             ########################
             ######## code ##########
             dev_accuracy = 0
-            print("Epoch: {}/10,  Train accuracy = {},  Val accuracy = {}".format(epoch, np.sum(accuracy)*100), np.sum(dev_accuracy)*100)
+            print("Epoch: {}/10,  Train accuracy = {},  Val accuracy = {}".format(epoch, np.sum(accuracy)*100, np.sum(dev_accuracy)*100))
             
 
 
@@ -119,8 +119,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoint_path", help="Folder(name) where checkpoints are saved. If it is a regex expression, all experiments are evaluated", type=str, default = './checkpoints')
     parser.add_argument("--overwrite", help="Whether evaluations should be re-run if there already exists an evaluation file.", action="store_true")
-    parser.add_argument("--visualize_embeddings", help="Whether the embeddings of the model should be visualized or not", action="store_true")
-    parser.add_argument("--full_senteval", help="Whether to run SentEval with the heavy setting or not", action="store_true")
+    
     args = parser.parse_known_args()[0]
     model_list = sorted(glob(args.checkpoint_path))
 